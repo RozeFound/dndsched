0da7382f7d326053e05530f118ea4db9fc256125
RozeFound <rozefound@proton.me>
2024-09-04 11:09:47 +0700

    Replace hours long timeout with regular checks Change naming

--- extension.js
+++ extension.js
@@ -25,91 +25,83 @@ export default class DnDExtension extends Extension {
 
     enable() {
 
-        this._notification_settings = new Gio.Settings({
+        this.__notification_settings = new Gio.Settings({
             schema_id: "org.gnome.desktop.notifications",
         })
 
-        this.__dnd = !this._notification_settings.get_boolean('show-banners');
+        this.__old_dnd = !this.__notification_settings.get_boolean('show-banners');
+        this.__current_value = this.__old_dnd;
 
-        this._settings = this.getSettings('org.gnome.shell.extensions.dndsched');
+        this.__settings = this.getSettings('org.gnome.shell.extensions.dndsched');
 
-        this._reschedule();
+        this._enable_if_needed();
+        
+        this. __check_tid = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, this._regular_check.bind(this));
 
-        this.__settings_enable_tid = this._settings.connect('changed::enable-dnd-time-offset', () => {
-            this._reschedule();
-        });
-
-        this.__settings_disable_tid = this._settings.connect('changed::disable-dnd-time-offset', () => {
-            this._reschedule();
-        });
+        this.__settings_enable_cid = this.__settings.connect('changed::enable-dnd-time-offset', this._enable_if_needed.bind(this));
+        this.__settings_disable_cid = this.__settings.connect('changed::disable-dnd-time-offset', this._enable_if_needed.bind(this));
 
     }
 
-    _reschedule() {
+    _regular_check() {
+        this._enable_if_needed();
+        return GLib.SOURCE_CONTINUE;
+    }
 
-        let time = Math.floor(this._get_time() / 60)
-        let enable_time = this._settings.get_int('enable-dnd-time-offset');
-        let disable_time = this._settings.get_int('disable-dnd-time-offset');
+    _enable_if_needed() {
+
+        let time = this._get_time();
+        let enable_time = this.__settings.get_int('enable-dnd-time-offset');
+        let disable_time = this.__settings.get_int('disable-dnd-time-offset');
 
         let dnd = ((enable_time < time && time < disable_time) || 
                   (enable_time > disable_time &&
                   (time <= disable_time || time >= enable_time)))
         
         this._set_dnd(dnd);
-
-        this._cleanup();
         
-        this._enable_tid = this._sched_dnd(enable_time, true);
-        this._disable_tid = this._sched_dnd(disable_time, false);
-
     }
-
-    _sched_dnd(_time, _value) {
-        return GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT,
-            Math.abs((_time * 60) - this._get_time()),
-            ()=> { this._set_dnd(_value); } );
-    }
-
-    _set_dnd(_value) {
-        this._notification_settings.set_boolean('show-banners', !_value);
+    
+    _set_dnd(value) {
+        if (this.__current_value != value) {
+            this.__current_value = value;
+            this.__notification_settings.set_boolean('show-banners', !value);
+        }
     }
 
     _get_time() {
-        let _time = new Date();
-        return (_time.getHours() * 3600) + (_time.getMinutes() * 60) + _time.getSeconds();
+        let time = new Date();
+        return (time.getHours() * 60) + time.getMinutes();
     }
 
     _cleanup() {
-        if (this._enable_tid) {
-            GLib.Source.remove(this._enable_tid);
-            this._enable_tid = null;
-        }
-
-        if (this._disable_tid) {
-            GLib.Source.remove(this._disable_tid);
-            this._disable_tid = null;
+        if (this.__check_tid) {
+            GLib.source_remove(this.__check_tid);
+            this.__check_tid = null;
         }
     }
 
     disable() {
 
-        if (this.__settings_enable_tid) {
-            this._settings.disconnect(this.__settings_enable_tid);
-            this.__settings_enable_tid = null;
+        if (this.__settings_enable_cid) {
+            this.__settings.disconnect(this.__settings_enable_cid);
+            this.__settings_enable_cid = null;
         }
 
-        if (this.__settings_disable_tid) {
-            this._settings.disconnect(this.__settings_disable_tid);
-            this.__settings_disable_tid = null;
+        if (this.__settings_disable_cid) {
+            this.__settings.disconnect(this.__settings_disable_cid);
+            this.__settings_disable_cid = null;
         }
 
         this._cleanup();
         
-        this._set_dnd(this.__dnd);
+        this._set_dnd(this.__old_dnd);
 
         this.__dnd = null;
-        this._settings = null;
-        this._notification_settings = null;
+        this.__current_value = null;
+
+        this.__settings = null;
+        this.__notification_settings = null;
         
     }
 }
--- prefs.js
+++ prefs.js
@@ -41,18 +41,17 @@ export default class DnDPreferences extends ExtensionPreferences {
         group.add(this._add_timerow(_('Enable DnD after'), 'enable-dnd-time-offset'));
         group.add(this._add_timerow(_('Disable DnD after'), 'disable-dnd-time-offset'));
 
-        window._settings = this.getSettings();
     }
 
-    _add_timerow(_label, _settings_id) {
+    _add_timerow(label, settings_id) {
 
-        let _settings = this.getSettings();
+        let settings = this.getSettings();
 
-        let _time = _settings.get_int(_settings_id);
-        let _hours = Math.floor(_time / 60)
-        let _minutes = _time % 60;
+        let time = settings.get_int(settings_id);
+        let hours = Math.floor(time / 60);
+        let minutes = time % 60;
 
-        let row = new Adw.ActionRow({ title: _label });
+        let row = new Adw.ActionRow({ title: label });
 
         const hours_spin = new Gtk.SpinButton({
             adjustment: new Gtk.Adjustment({
@@ -68,11 +67,11 @@ export default class DnDPreferences extends ExtensionPreferences {
             valign: Gtk.Align.CENTER,
         });
 
-        hours_spin.set_value(_hours);
+        hours_spin.set_value(hours);
 
         hours_spin.connect('changed', () => {
-            _hours = hours_spin.get_value_as_int();
-            _settings.set_int(_settings_id, _hours * 60 + _minutes);
+            hours = hours_spin.get_value_as_int();
+            settings.set_int(settings_id, hours * 60 + minutes);
         });
 
         const minutes_spin = new Gtk.SpinButton({
@@ -89,11 +88,11 @@ export default class DnDPreferences extends ExtensionPreferences {
             valign: Gtk.Align.CENTER,
         });
 
-        minutes_spin.set_value(_minutes);
+        minutes_spin.set_value(minutes);
 
         minutes_spin.connect('changed', () => {
-            _minutes = minutes_spin.get_value_as_int();
-            _settings.set_int(_settings_id, _hours * 60 + _minutes);
+            minutes = minutes_spin.get_value_as_int();
+            settings.set_int(settings_id, hours * 60 + minutes);
         });
 
         row.add_suffix(hours_spin);
